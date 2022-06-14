ec6c7f90ed23108b4377d2ef783b5b694178c816
K. Suryaa Narayanan <72430187+Suryaa07@users.noreply.github.com>
2022-06-14 11:20:00 +0530

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,14 @@
+# COLORS (r, g, b)
+WHITE = (238, 60, 251)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+BGCOLOUR = DARKGREY
+
+# game settings
+WIDTH = 800
+HEIGHT = 641
+FPS = 60
+title = "Sliding Puzzle Game"
+TILESIZE = 128
+GAME_SIZE = 3
